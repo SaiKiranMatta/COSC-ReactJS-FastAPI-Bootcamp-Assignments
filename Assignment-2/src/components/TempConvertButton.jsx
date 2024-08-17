a2efcf418332a2ab4abf559aed1239186a96e1b5
SaiKiranMatta <saikirangoud.matta@gmail.com>
2024-08-18 04:31:39 +0530

    Add Assignment 2

---
+++ Assignment-2/src/components/TempConvertButton.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TempConvertButton = ({ convertTemperature }) => {
+    return <button onClick={convertTemperature}>Convert to </button>;
+};
+
+export default TempConvertButton;
